1c3ac0d5f5c5f3d54200310f2dda6171dc9f8d09
mbanerjeepalmer <mbanerjeepalmer@gmail.com>
2022-08-26 18:19:10 +0100

    Transfer-focused content

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,13 +1,13 @@
 <div class="pt-24">
-    <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+    <div class="container px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <!--Left Col-->
       <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-        <p class="uppercase tracking-loose w-full">MauPay payments</p>
+        <!-- <p class="uppercase tracking-loose w-full">MauPay payments</p> -->
         <h1 class="my-4 text-5xl font-bold leading-tight">
-          Putting the pay into MauPay
+          Buy Now, Maupay Later
         </h1>
         <p class="leading-normal text-2xl mb-8">
-          The wait is over. The world's only Neal Maupay-focused payments solution.
+          Buy Neal Maupay for your club using MauPay.
         </p>
         <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
           Join the waitlist
