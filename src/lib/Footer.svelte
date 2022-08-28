f17369f09551517d95149b4b6ea48a4503e9dc89
mbanerjeepalmer <mbanerjeepalmer@gmail.com>
2022-08-28 15:51:15 +0100

    Finishing touches to the content

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,21 +1,17 @@
 <footer class="bg-white">
   <div class="container mx-auto px-8">
-    <div class="w-full flex flex-col md:flex-row py-6">
-      <div class="flex-1 mb-6 text-black">
-        <a
-          class="text-blue-800 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+    <div class="w-full flex flex-col md:flex-row py-6 md:space-x-2">
+      <div class="flex flex-col flex-1 mb-6 self-start text-gray-700">
+      <div class="flex flow-row items-center space-x-2">
+        <img alt="NealCoin" class="max-h-14" src="./Face coin.svg"/>
+        <span
+          class="text-blue-800 no-underline hover:no-underline font-bold text-4xl"
           href="#"
         >
-          <!--Icon from: http://www.potlabicons.com/ -->
-          <!-- <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
-              <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
-              <path
-                class="plane-take-off"
-                d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
-              />
-            </svg> -->
           MauPay
-        </a>
+        </span>
+	</div>
+	  <span class="mt-6 text-sm inline-block mr-2 md:block md:mr-0 text-gray-500">MauPay is not affiliated with Neal Maupay the footballer. It's a cutting-edge, red-hot, hypergrowth fintech which pays homage to the ex-Brighton striker.</span>
       </div>
       <div class="flex-1">
         <p class="uppercase text-gray-500 md:mb-6">Links</p>
@@ -112,13 +108,15 @@
               >Contact</a
             >
           </li>
-          <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+          <li class="mt-2 inline-block mr-2 md:block md:mr-0 text-sm">
             <a
               href="https://www.freepik.com/free-photos-vectors/background"
-              class="text-gray-500"
+              class="text-gray-600"
               >Background vector created by freepik - www.freepik.com</a
             >
           </li>
+	  <li>
+	  </li>
         </ul>
       </div>
     </div>
--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -5,7 +5,7 @@ import Email from "./Email.svelte";
 <div class="pt-24">
     <div class="container px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <!--Left Col-->
-      <div class="flex flex-col w-full md:w-2/5 justify-center items-center text-center md:text-left">
+      <div class="flex flex-col w-full md:w-2/5 text-center md:text-left">
         <p class="uppercase tracking-loose w-full">Probably on the blockchain</p>
         <h1 class="my-4 text-5xl font-bold leading-tight">
           Get the MauPay wallet
@@ -13,7 +13,9 @@ import Email from "./Email.svelte";
         <p class="leading-normal text-2xl mb-8">
           Put the pay into Maupay.
         </p>
+	<div class="place-self-center md:place-self-start">
         <Email/>
+	</div>
         <!-- <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
           Join the waitlist
         </button> -->
